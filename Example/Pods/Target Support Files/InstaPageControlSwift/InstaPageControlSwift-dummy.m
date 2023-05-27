8feb1ef5a8a45710e0aea1ffb58fbf643f5e6d85
wajeeh <wajeeh661@gmail.com>
2023-05-27 16:15:22 +0500

    Release 0.0.1

---
+++ Example/Pods/Target Support Files/InstaPageControlSwift/InstaPageControlSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_InstaPageControlSwift : NSObject
+@end
+@implementation PodsDummy_InstaPageControlSwift
+@end
---
+++ Example/Pods/Target Support Files/Pods-InstaPageControlSwift_Example/Pods-InstaPageControlSwift_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InstaPageControlSwift_Example : NSObject
+@end
+@implementation PodsDummy_Pods_InstaPageControlSwift_Example
+@end
